2376884bdb03c4d7195915727874e1df162d3931
replacementorange <notnenar@proton.me>
2024-05-23 21:15:20 +0300

    3.2: Phonebook backend step 2

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -28,8 +28,17 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
 
-  app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response) => {
     response.json(persons)
+  })
+
+app.get('/info', (request, response) => {
+    const people = persons.length
+    const date = new Date
+    response.send(`\
+      <p>Phonebook has info for ${people} people
+      <br/> ${date}</p>
+    `)
   })
 
 const PORT = 3001
